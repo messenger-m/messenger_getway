0f532f88b6938f64375ca2105044b5f7fb695aa8
Artem Morozov <morozec76@gmail.com>
2025-12-31 23:37:00 +0300

    add login

--- src/handlers/auth.go
+++ src/handlers/auth.go
@@ -1,9 +1,16 @@
 package handlers
 
 import (
+	redis_handler "api-gateway/src/redisHandler"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/redis/go-redis/v9"
 )
 
 // @Summary      Auth
@@ -23,8 +30,36 @@ func LoginHandler(c *fiber.Ctx) error {
 
 	fmt.Printf("Received login request: %+v\n", req)
 
-	return c.JSON(fiber.Map{
-		"status": "ok",
-		"login":  req.Login,
-	})
+	// Сериализуем req в JSON
+	reqBody, err := json.Marshal(req)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString("JSON marshal error")
+	}
+
+	// Делаем POST-запрос
+	resp, err := http.Post("http://127.0.0.1:8000/auth/add_user", "application/json", bytes.NewBuffer(reqBody))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString("Auth service error")
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString("Read response error")
+	}
+	fmt.Printf("Auth service response: %s\n", string(body))
+
+	_, err = redis_handler.Client.XAdd(redis_handler.Ctx, &redis.XAddArgs{
+		Stream: "events:in",
+		Values: map[string]interface{}{
+			"type": "login",
+			"text": string(body),
+		},
+	}).Result()
+
+	if err != nil {
+		log.Println("Redis error:", err)
+	}
+
+	return c.Status(resp.StatusCode).Send(body)
 }
